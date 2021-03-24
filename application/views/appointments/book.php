cf83d9981886203221fee273c7462e00f06a627f
David Kagerer <david@binder-kagerer.de>
2021-03-24 23:14:51 +0100

    feat: tidy up the book.php
    separate header and progressbar in own files

--- application/views/appointments/book.php
+++ application/views/appointments/book.php
@@ -29,30 +29,8 @@
 
                 <!-- FRAME TOP BAR -->
 
-                <div id="header">
-                    <span id="company-name"><?= $company_name ?></span>
+                <?php include "book/header.php"; ?>
 
-                    <div id="steps">
-                        <div id="step-1" class="book-step active-step" data-tippy-content="<?php
-                                                                                            if ($display_any_provider == true || count($available_providers) > 1 || $display_one_provider == true) {
-                                                                                                echo lang('service_and_provider');
-                                                                                            } else {
-                                                                                                echo lang('service');
-                                                                                            } ?>">
-                            <strong>1</strong>
-                        </div>
-
-                        <div id="step-2" class="book-step" data-toggle="tooltip" data-tippy-content="<?= lang('appointment_date_and_time') ?>">
-                            <strong>2</strong>
-                        </div>
-                        <div id="step-3" class="book-step" data-toggle="tooltip" data-tippy-content="<?= lang('customer_information') ?>">
-                            <strong>3</strong>
-                        </div>
-                        <div id="step-4" class="book-step" data-toggle="tooltip" data-tippy-content="<?= lang('appointment_confirmation') ?>">
-                            <strong>4</strong>
-                        </div>
-                    </div>
-                </div>
 
                 <?php if ($manage_mode) : ?>
                     <div id="cancel-appointment-frame" class="row booking-header-bar">
---
+++ application/views/appointments/book/header.php
@@ -0,0 +1,6 @@
+<div id="header">
+    <div id="company-name" class="navbar-brand">
+        <img src="<?= base_url('assets/img/logo.png') ?>">
+    </div>
+    <?php include "progress-bar.php" ?>
+</div>
